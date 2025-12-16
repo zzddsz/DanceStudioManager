0e073a959795ca09ff8f210e5d06cb1e091980f8
zzddsz <me5789588@gmail.com>
2025-12-16 09:27:24 -0300

    mod 2.0

--- DanceStudioManager/Forms/FormEnrollmentList.Designer.cs
+++ DanceStudioManager/Forms/FormEnrollmentList.Designer.cs
@@ -16,73 +16,96 @@
 
         private void InitializeComponent()
         {
-            this.panelTop = new System.Windows.Forms.Panel();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.btnDelete = new System.Windows.Forms.Button();
-            this.dgvEnrollments = new System.Windows.Forms.DataGridView();
-            this.panelTop.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvEnrollments)).BeginInit();
-            this.SuspendLayout();
-
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            panelTop = new Panel();
+            btnAdd = new Button();
+            btnDelete = new Button();
+            dgvEnrollments = new DataGridView();
+            panelTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvEnrollments).BeginInit();
+            SuspendLayout();
+            // 
             // panelTop
-            this.panelTop.BackColor = System.Drawing.Color.FromArgb(128, 64, 64);
-            this.panelTop.Controls.Add(this.btnAdd);
-            this.panelTop.Controls.Add(this.btnDelete);
-            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTop.Location = new System.Drawing.Point(0, 0);
-            this.panelTop.Name = "panelTop";
-            this.panelTop.Size = new System.Drawing.Size(800, 80);
-            this.panelTop.TabIndex = 0;
-
+            // 
+            panelTop.BackColor = Color.FromArgb(128, 64, 64);
+            panelTop.Controls.Add(btnAdd);
+            panelTop.Controls.Add(btnDelete);
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Location = new Point(0, 0);
+            panelTop.Name = "panelTop";
+            panelTop.Size = new Size(800, 80);
+            panelTop.TabIndex = 0;
+            // 
             // btnAdd
-            this.btnAdd.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnAdd.Location = new System.Drawing.Point(163, 12);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(235, 52);
-            this.btnAdd.TabIndex = 0;
-            this.btnAdd.Text = "Add Enrollment";
-            this.btnAdd.UseVisualStyleBackColor = true;
-
+            // 
+            btnAdd.Anchor = AnchorStyles.Top;
+            btnAdd.Location = new Point(163, 12);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(235, 52);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "Add Enrollment";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
             // btnDelete
-            this.btnDelete.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnDelete.Location = new System.Drawing.Point(424, 12);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(242, 52);
-            this.btnDelete.TabIndex = 1;
-            this.btnDelete.Text = "Remove Enrollment";
-            this.btnDelete.UseVisualStyleBackColor = true;
-
+            // 
+            btnDelete.Anchor = AnchorStyles.Top;
+            btnDelete.Location = new Point(424, 12);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(242, 52);
+            btnDelete.TabIndex = 1;
+            btnDelete.Text = "Remove Enrollment";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
             // dgvEnrollments
-            this.dgvEnrollments.AllowUserToAddRows = false;
-            this.dgvEnrollments.AllowUserToDeleteRows = false;
-            this.dgvEnrollments.AllowUserToResizeRows = false;
-            this.dgvEnrollments.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dgvEnrollments.BackgroundColor = System.Drawing.Color.White;
-            this.dgvEnrollments.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.dgvEnrollments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvEnrollments.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvEnrollments.Location = new System.Drawing.Point(0, 80);
-            this.dgvEnrollments.MultiSelect = false;
-            this.dgvEnrollments.Name = "dgvEnrollments";
-            this.dgvEnrollments.ReadOnly = true;
-            this.dgvEnrollments.RowHeadersVisible = false;
-            this.dgvEnrollments.RowHeadersWidth = 51;
-            this.dgvEnrollments.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvEnrollments.Size = new System.Drawing.Size(800, 370);
-            this.dgvEnrollments.TabIndex = 1;
-
+            // 
+            dgvEnrollments.AllowUserToAddRows = false;
+            dgvEnrollments.AllowUserToDeleteRows = false;
+            dgvEnrollments.AllowUserToResizeRows = false;
+            dgvEnrollments.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvEnrollments.BackgroundColor = Color.White;
+            dgvEnrollments.BorderStyle = BorderStyle.None;
+            dgvEnrollments.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Window;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(128, 64, 64);
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.False;
+            dgvEnrollments.DefaultCellStyle = dataGridViewCellStyle1;
+            dgvEnrollments.Dock = DockStyle.Fill;
+            dgvEnrollments.Location = new Point(0, 80);
+            dgvEnrollments.MultiSelect = false;
+            dgvEnrollments.Name = "dgvEnrollments";
+            dgvEnrollments.ReadOnly = true;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = Color.DodgerBlue;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dgvEnrollments.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dgvEnrollments.RowHeadersVisible = false;
+            dgvEnrollments.RowHeadersWidth = 51;
+            dgvEnrollments.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvEnrollments.Size = new Size(800, 370);
+            dgvEnrollments.TabIndex = 1;
+            // 
             // FormEnrollmentList
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dgvEnrollments);
-            this.Controls.Add(this.panelTop);
-            this.Name = "FormEnrollmentList";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Gerenciar Matrículas";
-            this.panelTop.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvEnrollments)).EndInit();
-            this.ResumeLayout(false);
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dgvEnrollments);
+            Controls.Add(panelTop);
+            Name = "FormEnrollmentList";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Gerenciar Matrículas";
+            panelTop.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvEnrollments).EndInit();
+            ResumeLayout(false);
         }
     }
 }
