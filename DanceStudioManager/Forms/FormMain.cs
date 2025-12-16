91592b016d544c77cc65fa6e72371a07c2235475
zzddsz <me5789588@gmail.com>
2025-12-16 08:36:01 -0300

    mod

--- DanceStudioManager/Forms/FormMain.cs
+++ DanceStudioManager/Forms/FormMain.cs
@@ -1,12 +1,13 @@
 ﻿using DanceStudioManager.Forms;
 using Microsoft.Extensions.DependencyInjection;
+using ReaLTaiizor.Forms;
 using System;
 using System.Drawing;
 using System.Windows.Forms;
 
 namespace DanceStudioManager
 {
-    public partial class FormMain : Form
+    public partial class FormMain : MaterialForm
     {
         private readonly IServiceProvider _provider;
 
